28eb33da57a6bf14556619cc5c1d8078bf9ebc81
hinddaoud <hhinddaoud@gmail.com>
2022-10-20 18:22:29 +0300

    Portfolio

--- main.js
+++ main.js
@@ -1,22 +1,63 @@
+// 
+const lodAbout=()=>{
+
+   $("#mainSkillDiv").hide();
+     $("#abputDiv").show();
+     $('.projectClass').hide()
+     $('.gallery1').hide()
+}
+lodAbout()
 $("#liskill").click(function(){
      $("#mainSkillDiv").show();
      $("#abputDiv").hide();
      $('.projectClass').hide()
+     $('.gallery1').hide()
   });
 
 
-
+  
   $("#liAboutDiv").click(function(){
     $("#mainSkillDiv").hide();
      $("#abputDiv").show();
      $('.projectClass').hide()
+     $('.gallery1').hide()
   });
 
-
+  $(".hospital").click(function(){
+   $("#mainSkillDiv").hide();
+    $("#abputDiv").hide();
+    $('.projectClass').hide()
+    $('.gallery1').show()
+   
+ });
   
   $("#projectdiv").click(function(){
    $("#mainSkillDiv").hide();
     $("#abputDiv").hide();
     $('.projectClass').show()
+    $('.gallery1').hide()
  });
   
+
+ //Drag and drop Sortable.js
+new Sortable(gallery, {
+   swapThreshold: 1,
+   animation: 150
+ });
+ 
+ //Add random heart
+ function heart_loop() {
+   var number = 1 + Math.floor(Math.random() * 6);
+   if (number == 1 || number == 5 || number == 6) {
+     $("#" + number).addClass("heart dark");
+   } else {
+     $("#" + number).addClass("heart");
+   }
+   setTimeout(function () {
+     $("#" + number).removeClass();
+     heart_loop();
+   }, 5000);
+ }
+ 
+ heart_loop();
+ 
